40a98fb9bd196a8950a6e44794b05fb49c94a0ac
Babacar DIOUF <Diouf.Pape.Babacar@gmail.com>
2013-02-19 14:07:35 +0100

    MAJ de la version audio - OK - 19/02/2013 à 14h07 |

--- src/controller/AboutController.java
+++ src/controller/AboutController.java
@@ -8,6 +8,6 @@ public class AboutController {
 
   public static String Text = "This is an ErrViz program which can help you to"
           + " detect errors in automatic speech recognition systems";
-  public static String Version = "0.1.0";
+  public static String Version =  "0.1.0";
   public static String Dev = "Developed by LIUM";
 }
